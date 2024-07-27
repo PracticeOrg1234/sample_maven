e682a1bf2f4ac17aa6e8461bc9aed9188cc6b344
root <root@ip-172-31-9-27.ap-south-1.compute.internal>
2024-07-27 14:51:01 +0000

    First commit

---
+++ src/main/java/JavaProjetc/App.java
@@ -0,0 +1,13 @@
+package JavaProjetc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
